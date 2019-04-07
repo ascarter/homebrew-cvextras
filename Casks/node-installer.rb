60bd7925a5044e369392abfe824cb03b3934997d
Andrew Carter <asc@snoq.io>
2019-04-07 09:29:39 -0700

    And Node.js 11.13.0
    Use installer for Node.js
    Version 11.13.0

---
+++ Casks/node-installer.rb
@@ -0,0 +1,15 @@
+cask 'node-installer' do
+  version '11.13.0'
+  sha256 '255ef8b33f622bf2377cd3acc4197f232cd120b60143369cb6ff88dc939d0419'
+
+  url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
+  name 'Node.js'
+  homepage 'https://nodejs.org'
+
+  pkg "node-v#{version}.pkg"
+  
+  uninstall pkgutil: [
+    'org.nodejs.npm.pkg',
+    'org.nodejs.node.pkg'
+  ]
+end
