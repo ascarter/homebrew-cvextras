983d678e1692bcd1aa2c53654bc69f654808b27f
Andrew Carter <asc@snoq.io>
2019-04-07 20:35:59 -0700

    Use github releases appcast for Node.js

--- Casks/node-installer.rb
+++ Casks/node-installer.rb
@@ -3,7 +3,7 @@ cask 'node-installer' do
   sha256 '255ef8b33f622bf2377cd3acc4197f232cd120b60143369cb6ff88dc939d0419'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
-  appcast 'https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V11.md'
+  appcast 'https://github.com/nodejs/node/releases.atom'
   name 'Node.js'
   homepage 'https://nodejs.org'
 
