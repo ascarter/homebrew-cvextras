6e9a71ac7339079b3fdb34acf870e0b168c090e2
Andrew Carter <asc@snoq.io>
2019-04-07 20:35:31 -0700

    Update Go appcast to use GitHub releases

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -3,7 +3,7 @@ cask 'go-installer' do
   sha256 '9b191761ff2d7327aaccefa8679b5e722a513c14d1f4a6ed74a9429f8d1339a7'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
-  appcast 'https://golang.org/doc/devel/release.html'
+  appcast 'https://github.com/golang/go/releases.atom'
   name 'Go Programming Language'
   homepage 'https://golang.org'
 
