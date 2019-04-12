70914bbe58e74a32b981d1544160bdb2d9509611
Andrew Carter <acarter@flipboard.com>
2019-04-11 19:51:18 -0700

    Update Go 1.12.4

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.12.3'
-  sha256 '325b7eba71bf705e12bea3dc8a5cd3995b788cba3b6a97c93157fdfac990058f'
+  version '1.12.4'
+  sha256 '5b3c6aafd31746809e2776c685e89190f85307074e48af2681d472f8a6a3c504'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
