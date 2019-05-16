0c70d9a6146edd06c955474479d930a1ade721a7
Andrew Carter <asc@snowridge.io>
2019-05-16 15:54:36 -0700

    Update NodeJS to 12.2.0

--- Casks/node-installer.rb
+++ Casks/node-installer.rb
@@ -1,6 +1,6 @@
 cask 'node-installer' do
-  version '11.13.0'
-  sha256 '255ef8b33f622bf2377cd3acc4197f232cd120b60143369cb6ff88dc939d0419'
+  version '12.2.0'
+  sha256 '80a45502c1a5810f6ae87935b2a9f1612d4cd76cdccddc490dca6d0cbdfa8c3d'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   appcast 'https://github.com/nodejs/node/releases.atom'
