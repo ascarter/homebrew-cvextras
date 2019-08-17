357d359874563daf33424f2594402734ccbaaefa
Andrew Carter <acarter@flipboard.com>
2019-08-17 08:35:10 -0700

    Update Go 1.12.9

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.12.8'
-  sha256 '6e879a183b904273c3e3c7e4935ddecb1f7d298461648852529496499d861e41'
+  version '1.12.9'
+  sha256 '4f5478e6d985f4f68099e03f4485149efa697cf81610e514974291a29ee7e21b'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
@@ -10,6 +10,6 @@ cask 'go-installer' do
   depends_on macos: '>= :yosemite'
 
   pkg "go#{version}.darwin-amd64.pkg"
-  
+
   uninstall pkgutil: 'com.googlecode.go'
 end
