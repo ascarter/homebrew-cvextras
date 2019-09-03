f3b79b89423ee26ae98951d6a44d42bdfbd14039
Andrew Carter <acarter@flipboard.com>
2019-09-03 12:24:00 -0700

    Update Go 1.13

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.12.9'
-  sha256 '4f5478e6d985f4f68099e03f4485149efa697cf81610e514974291a29ee7e21b'
+  version '1.13'
+  sha256 '7dc05b582ed8730b7f188af55ca68500dd1eaa267f0f84e46751858360d2b665'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
