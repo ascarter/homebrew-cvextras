79618d32ae63c8eb420ac4355c2e820596243904
Andrew Carter <ascarter@uw.edu>
2019-10-12 10:52:49 -0700

    Update Node 12.12.0

--- Casks/node-installer.rb
+++ Casks/node-installer.rb
@@ -1,6 +1,6 @@
 cask 'node-installer' do
-  version '12.9.0'
-  sha256 '7a18632a187137f21f88dbc0ae85c8a34d20fccb4b7c26be665fdc56a929c3fc'
+  version '12.12.0'
+  sha256 'e6d1988c241fef87d9672742a924c94ecbcefa2fe622c937791853eaf652ddaa'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   appcast 'https://github.com/nodejs/node/releases.atom'
