2cd9c3a046313f91c263a0fecd951e73795b57e1
Andrew Carter <ascarter@uw.edu>
2019-12-06 20:48:20 -0800

    Update Go 1.13.5

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.13.3'
-  sha256 '75f39b2d2b98ab7fe262252cb3f87b14bce71c16df8b9df7ae0fb1db1ba2576c'
+  version '1.13.5'
+  sha256 'b973bc34f146d20a168753cbc151979504bc76ddafdeae86016235911902b481'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
