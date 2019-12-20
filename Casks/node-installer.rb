8f6d3b383116888ac9f6becf3089da943519e950
Andrew Carter <ascarter@uw.edu>
2019-12-20 08:44:32 -0800

    Update Node.js 13.5.0

--- Casks/node-installer.rb
+++ Casks/node-installer.rb
@@ -1,6 +1,6 @@
 cask 'node-installer' do
-  version '13.3.0'
-  sha256 '8a76651fa9ff4226482e82a2cea37a1e972d4e411e405548b17b25702f8e4a40'
+  version '13.5.0'
+  sha256 'b838c1ad285a2407982061430ad42167c4fccb042620e0a9db70950dfba6e238'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   appcast 'https://github.com/nodejs/node/releases.atom'
