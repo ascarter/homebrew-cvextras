8593c3af9c47da7770111192bbea12067e0b4017
Andrew Carter <ascarter@uw.edu>
2020-02-26 21:41:52 -0800

    Update Go 1.14

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.13.5'
-  sha256 'b973bc34f146d20a168753cbc151979504bc76ddafdeae86016235911902b481'
+  version '1.14'
+  sha256 '8405d748b7b56720b004c18087576a24b7eab057725c03c8160b949d239c6ee4'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
