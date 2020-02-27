7560d6f5c144c30070b84406b981fcf2890a4ae8
Andrew Carter <ascarter@uw.edu>
2020-02-26 22:38:06 -0800

    Update Python 3.8.2

--- Casks/python-installer.rb
+++ Casks/python-installer.rb
@@ -1,6 +1,6 @@
 cask 'python-installer' do
-  version '3.8.0'
-  sha256 '30961fe060da9dc5afdc4e789a57fe9bcc0d20244474e9f095d7bfc89d2e1869'
+  version '3.8.2'
+  sha256 '7254ed07ceb8b02b35297b555abd660db89519407b43abb05d5ea57570d4182e'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.9.pkg"
   name 'Python Programming Language'
@@ -18,9 +18,9 @@ cask 'python-installer' do
     ]
 
   uninstall pkgutil: [
-    'org.python.Python.PythonFramework-3.7',
-    'org.python.Python.PythonDocumentation-3.7',
-    'org.python.Python.PythonApplications-3.7',
-    'org.python.Python.PythonUnixTools-3.7'
+    'org.python.Python.PythonFramework-3.8',
+    'org.python.Python.PythonDocumentation-3.8',
+    'org.python.Python.PythonApplications-3.8',
+    'org.python.Python.PythonUnixTools-3.8'
   ]
 end
