0564efbaf0b2ab8a4ac4506c9073acd3594a7d8e
Andrew Carter <ascarter@uw.edu>
2020-05-22 12:22:52 -0700

    Update Node.js 14.3.0

--- Casks/node-installer.rb
+++ Casks/node-installer.rb
@@ -1,6 +1,6 @@
 cask 'node-installer' do
-  version '13.12.0'
-  sha256 'd78bdd6bc5f3727189cb733e035c0e07b649248b065bc677aedfc476f0e8307d'
+  version '14.3.0'
+  sha256 'bcb0574454a1646cce32425b927dd0b032c7e8a8c089d1ce1db9b44546c3aa6f'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   appcast 'https://github.com/nodejs/node/releases.atom'
