3e77b64ec012f843a7f9e9a8c5f58ccda5b5633d
Andrew Carter <asc@alpental.io>
2020-06-14 11:30:42 -0700

    Update Go 14.4.0

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,13 +1,11 @@
 cask 'go-installer' do
-  version '1.14.3'
-  sha256 '05cb5531fd429aa5a13605a30aed5584d95a134d6edbf5e9ac174156390d423a'
+  version '1.14.4'
+  sha256 'b518f21f823759ee30faddb1f623810a432499f050c9338777523d9c8551c62c'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
   name 'Go Programming Language'
   homepage 'https://golang.org'
-
-  depends_on macos: '>= :yosemite'
 
   pkg "go#{version}.darwin-amd64.pkg"
 
