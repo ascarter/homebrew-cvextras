5df687f1b32a4bdbf14c495a5c45b6d1b810d50e
Andrew Carter <asc@alpental.io>
2020-06-14 11:31:14 -0700

    Update Python 3.8.3

--- Casks/python3-installer.rb
+++ Casks/python3-installer.rb
@@ -1,12 +1,10 @@
-cask 'python-installer' do
-  version '3.8.2'
-  sha256 '7254ed07ceb8b02b35297b555abd660db89519407b43abb05d5ea57570d4182e'
+cask 'python3-installer' do
+  version '3.8.3'
+  sha256 'a563cdb78a7ac5e7879f5a6a8acb363583077bfb1d9fa1e2981b159adfba3cb5'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.9.pkg"
   name 'Python Programming Language'
   homepage 'https://python.org'
-
-  depends_on macos: '>= :mavericks'
 
   pkg "python-#{version}-macosx10.9.pkg",
     choices: [
