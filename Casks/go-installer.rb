717a2147392d30533331856d711a95a8c9fee927
Andrew Carter <asc@alpental.io>
2020-10-14 21:17:35 -0700

    Update Go 1.15.3

--- Casks/go-installer.rb
+++ Casks/go-installer.rb
@@ -1,6 +1,6 @@
 cask 'go-installer' do
-  version '1.14.5'
-  sha256 'a0c8ca21a101e183a59ee553dfbeda6beb798169bbe065ff854cce9e1a9fffb7'
+  version '1.15.3'
+  sha256 'd9fedc1f579fa60ee9efffdeed718d9a1a40f02ca1cb8640d8d01d7f472d4a56'
 
   url "https://dl.google.com/go/go#{version}.darwin-amd64.pkg"
   appcast 'https://github.com/golang/go/releases.atom'
