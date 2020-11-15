5374b146d018e7ede4fba54b74ac0ad9479ed365
Andrew Carter <asc@alpental.io>
2020-11-14 20:50:07 -0800

    Update Python 3.9.0

--- Casks/python3-installer.rb
+++ Casks/python3-installer.rb
@@ -1,6 +1,6 @@
 cask 'python3-installer' do
-  version '3.8.3'
-  sha256 'a563cdb78a7ac5e7879f5a6a8acb363583077bfb1d9fa1e2981b159adfba3cb5'
+  version '3.9.0'
+  sha256 'dadee1d10c1a8ed58bb64ec7409a48c9fe38599791eaaea29303ee59fb95dfeb'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.9.pkg"
   name 'Python Programming Language'
@@ -9,16 +9,16 @@ cask 'python3-installer' do
   pkg "python-#{version}-macosx10.9.pkg",
     choices: [
       {
-        'choiceIdentifier' => 'org.python.Python.PythonProfileChanges-3.7',
+        'choiceIdentifier' => 'org.python.Python.PythonProfileChanges-3.9',
         'choiceAttribute'  => 'selected',
         'attributeSetting' => 0
       }
     ]
 
   uninstall pkgutil: [
-    'org.python.Python.PythonFramework-3.8',
-    'org.python.Python.PythonDocumentation-3.8',
-    'org.python.Python.PythonApplications-3.8',
-    'org.python.Python.PythonUnixTools-3.8'
+    'org.python.Python.PythonFramework-3.9',
+    'org.python.Python.PythonDocumentation-3.9',
+    'org.python.Python.PythonApplications-3.9',
+    'org.python.Python.PythonUnixTools-3.9'
   ]
 end
